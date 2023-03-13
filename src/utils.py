2907fc658a89bb73046f1596840f0b4dbef0dbb4
ritesh <riteshsingh9827@gmail.com>
2023-03-14 01:08:52 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,12 +21,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params,cv=3,n_jobs=3,verbose=1,refit=False):
     try:
         report={}
         for i in range(len(models)):
-            model=list(models.values())[i]
             
+            model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model,para,cv=cv,n_jobs=n_jobs,verbose=verbose,refit=refit)
+            gs.fit(X_train,y_train)
+            
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
             
             y_train_pred=model.predict(X_train)
@@ -34,8 +42,9 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             train_model_score=r2_score(y_train, y_train_pred)
             test_model_score=r2_score(y_test, y_test_pred)
             
-            report[list(models.keys())[i]]=test_model_score
-            
+            report[list(models.keys())[i]]=(test_model_score,gs.best_params_)
+     
+
         return report
     except Exception as e:
         raise CustomException(e,sys)
